c4e6c2f2b2a04026384e4bbbf974cc71a8b3b781
Heorhii Okhrimenko <46089946+Heorhii90@users.noreply.github.com>
2019-03-13 14:41:29 +0200

    calculate/
    13.03

---
+++ calcNew.js
@@ -0,0 +1,59 @@
+"use strict"
+
+
+function getOperand(questions) {
+    let operand = prompt(questions, '');
+    if (!validNumber(operand)) {
+        alert('Введите число');
+        return getOperand(questions);
+    }
+    return operand;
+}
+
+
+function getAction(operator) {
+    let action = prompt(operator, '');
+    if (!validOperator(action)) {
+        alert('Введите один из предлагаемых операторов');
+        return getAction(operator);
+    }
+    return action;
+}
+
+
+function calculate(operandA, operandB, action) {
+    let result;
+    if (action === '+') {
+        result = Number(operandA) + Number(operandB);
+    } else if (action === '-') {
+        result = Number(operandA) - Number(operandB);
+    } else if (action === '*') {
+        result = Number(operandA) * Number(operandB);
+    } else if (action === '/') {
+        result = Number(operandA) / Number(operandB);
+    }
+    return result;
+}
+
+
+function validNumber(value) {
+    return !(isNaN(value) ||
+        value === '' ||
+        value === null);
+}
+
+
+function validOperator(value) {
+    return (value === '+' ||
+        value === '-' ||
+        value === '*' ||
+        value === '/');
+}
+
+
+const operandA = getOperand('введите превое число', '');
+const operandB = getOperand('введите второе число', '');
+const action = getAction('Введите оператор( + - * / )', '');
+const result = calculate(operandA, operandB, action)
+
+alert('Результат: ' + result);
